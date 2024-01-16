30a54112087cad77ceb02264810d87bcdf38f945
VJsong02 <victor.song.02@gmail.com>
2024-01-16 14:58:27 +0100

    code import seems to work

--- testing/convert.py
+++ testing/convert.py
@@ -1,3 +1,7 @@
+import os
+
+CODE_PATH = "./code"
+
 def replace_term(text, term, before, after=None):
     if term not in text:
         return text
@@ -11,6 +15,46 @@ def replace_term(text, term, before, after=None):
     return de_termed
 
 
+def get_code(codeinclude, tag):
+    output = []
+    for lang in ["Python", "Java_Generic"]:
+        ext = lang[:len(lang)//3].lower()
+        path = "/".join((
+            CODE_PATH, 
+            lang, 
+            codeinclude + "." + ext
+        ))
+        if not os.path.isfile(path):
+            continue
+        with open(path) as f:
+            snippet = [f"```{ext}"]
+            read = False
+            for line in f.read().splitlines():
+                if f"ODSATag: {tag}" in line:
+                    read = True
+                    continue
+                if read:
+                    if "ODSAendTag" in line:
+                        break
+                    snippet.append(line)
+            snippet.append("```")
+            output.append((lang.replace("Java_Generic", "Java"), "\n".join(snippet)))
+    return output
+
+def tabify(title, tabs):
+    # create a tab thingy with a given title and a list of pairs
+    output = ['{{< tabs "' + title + '" >}}']
+    for tab, content in tabs:
+        output.append('{{< tab "' + tab + '" >}}')
+        output.append(content)
+        output.append('{{< /tab >}}')
+    output.append("{{< /tabs >}}")
+
+    return "\n".join(output)
+
+# the time complexity of this code is abysmal but
+# "it's fine if it's temporary right"
+# also the memory complexity is unimprovable so beat it
 def convert(text=None, path=None):
     assert text or path, "Neither text nor a path was provided."
 
@@ -20,17 +64,19 @@ def convert(text=None, path=None):
 
     lines = text.splitlines()
 
-    i = 0
-    # deal with metadata first
-    while not lines[i].startswith(".. index::"):
-        # stuff
-        i += 1
-    lines = lines[i:]
-    i = 0
+    # strip rst metadata for now (may be required for JSAV/khan stuff later)
+    while not lines[0].startswith(".. avmetadata::"): lines.pop(0)
+    for _ in range(4): lines.pop(0)
 
-    # fix headings and stuff
+    page_title = None
+
+    # fix headings
+    i = 0
     while i < len(lines):
         if set(lines[i]) == set("="):
+            # assume the first main heading is the page title
+            # also assume that it is the only page title
+            page_title = lines[i - 1]
             lines[i - 1] = "# " + lines[i - 1]
             lines.pop(i)
         elif set(lines[i]) == set("-"):
@@ -38,12 +84,25 @@ def convert(text=None, path=None):
             lines.pop(i)
         i += 1
 
+    i = 0
+    while i < len(lines):
+        if lines[i].startswith(".. codeinclude:: "):
+            path = lines[i].split("codeinclude::")[1].strip()
+            tag = lines[i + 1].split(":tag:")[1].strip()
+            
+            lines[i] = tabify(tag, get_code(path, tag))
+            lines.pop(i + 1)
+        i += 1
+
     body = "\n".join(lines[2:])
 
     body = replace_term(body, "term", "**")
     body = replace_term(body, "math", "{{< katex >}}", "{{< /katex >}}")
-    body = body.replace("``", "`")
+    body = body.replace("```", "CODEBLOCK").replace("``", "`").replace("CODEBLOCK", "```")
 
-    return body
+    output = "+++\n" + f'title = "{page_title}"\n' + "+++\n\n" + body
+
+    return output
+
 
 open("test.md", "w").write(convert(path="test.rst"))
